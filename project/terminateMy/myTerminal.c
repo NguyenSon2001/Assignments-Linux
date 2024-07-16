8f5b8ea9e0e18f75745f64da613d0b2fdc6a8b81
NguyenSon2001 <son.ng21nd@gmail.com>
2024-07-16 08:03:06 -0700

    run a shell not using pipe

--- project/terminateMy/myTerminal.c
+++ project/terminateMy/myTerminal.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
@@ -17,6 +18,12 @@ int takeInput(char *str);
 int parsePipe(char *str, char **strpiped);
 int process_ex(char *str_input);
 int parseSpace(char *str, char **parsed);
+int runshell(char **parsed, int lenParsed);
+int ownCmdHandler(char **parsed);
+
+
+
+
 int main()
 {
     char inputStr[MAXCOM];
@@ -27,7 +34,8 @@ int main()
         if (takeInput(inputStr))
             continue;
         int ret = process_ex(inputStr);
-        if(ret == -1){
+        if (ret == -1)
+        {
             printf("\nRemove the trailing '|' to avoid parsing errors.\n");
         }
     }
@@ -42,16 +50,145 @@ int process_ex(char *str_input)
     int piped = 0;
 
     piped = parsePipe(str_input, strpiped);
+
     if (piped < 0)
         return -1;
-    else if (piped == 0){
-        int byte =  parseSpace(str_input, parsed);
+    else if (piped == 0)
+    {
+        int len = parseSpace(str_input, parsed);
+        if (ownCmdHandler(parsed))
+            return 0;
+        int ret = runshell(parsed, len);
         return 0;
     }
-    else{
-
+    else
+    {
     }
 }
+
+int redirect_output_to_file(char *pathname)
+{
+    int fd = open(pathname, O_CREAT | O_APPEND | O_WRONLY, 0644);
+    if (fd < 0)
+    {
+        printf("ERROR:can't open %s\n", pathname);
+        return -1;
+    }
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+
+    return 0;
+}
+
+int redirect_input_to_file(char *pathname)
+{
+    int fd = open(pathname, O_RDONLY, 0664);
+    if (fd < 0)
+    {
+        printf("ERROR:can't open %s\n", pathname);
+        return -1;
+    }
+    dup2(fd, STDIN_FILENO);
+    close(fd);
+
+    return 0;
+}
+
+int runshell(char **parsed, int lenParsed)
+{
+    char token[2];
+    // Forking a child
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        printf("\nFailed forking child..");
+        return -1;
+    }
+    else if (pid == 0)
+    {
+        for (int i = 0; i < lenParsed; i++)
+        {
+            if (strcmp(parsed[i], ">") == 0 || strcmp(parsed[i], "<") == 0)
+            {
+                strcpy(token, parsed[i]);
+                if (i + 1 < lenParsed)
+                {
+                    if (strcmp(parsed[i], ">") == 0)
+                        redirect_output_to_file(parsed[i + 1]);
+                    else if (strcmp(parsed[i], "<") == 0)
+                        redirect_input_to_file(parsed[i + 1]);
+
+                    for (int j = i; j < lenParsed - 2; j++)
+                    {
+                        parsed[j] = parsed[j + 2];
+                    }
+                    parsed[lenParsed - 2] = NULL; // Kết thúc mảng
+
+                    break;
+                }
+                else
+                {
+                    printf("No file after token '%s'\n", token);
+                    return -3;
+                }
+            }
+        }
+        if (execvp(parsed[0], parsed) < 0)
+        {
+            printf("\nCould not execute command..");
+        }
+        exit(0);
+    }
+    else
+    {
+        // waiting for child to terminate
+        wait(NULL);
+        return 0;
+    }
+}
+
+int ownCmdHandler(char **parsed)
+{
+    int NoOfOwnCmds = 3, i, switchOwnArg = 0;
+    char *ListOfOwnCmds[NoOfOwnCmds];
+    char *username;
+
+    ListOfOwnCmds[0] = "exit";
+    ListOfOwnCmds[1] = "cd";
+    ListOfOwnCmds[2] = "hello";
+
+    for (i = 0; i < NoOfOwnCmds; i++)
+    {
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0)
+        {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+
+    switch (switchOwnArg)
+    {
+    case 1:
+        printf("\nGoodbye\n");
+        exit(0);
+    case 2:
+        chdir(parsed[1]);
+        return 1;
+    case 3:
+        username = getenv("USER");
+        printf("\nHello %s.\nMind that this is "
+               "not a place to play around."
+               "\nUse help to know more..\n",
+               username);
+        return 1;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
 int parsePipe(char *str, char **strpiped)
 {
     strpiped[0] = strsep(&str, "|");
@@ -80,7 +217,8 @@ int parseSpace(char *str, char **parsed)
         if (strlen(parsed[i]) == 0)
             i--;
     }
-    if(i <= 0 ) return -1;
+    if (i <= 0)
+        return -1;
     return i;
 }
 
